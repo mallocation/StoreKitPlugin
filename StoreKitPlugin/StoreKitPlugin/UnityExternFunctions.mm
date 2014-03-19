cb2475f4245cb66222427eae4bdb21f3bd1b82c0
Curtis Johnson <curtisdavidjohnson@gmail.com>
2014-03-18 20:07:47 -0500

    adding implementation for UnitySendMessage

---
+++ StoreKitPlugin/StoreKitPlugin/UnityExternFunctions.mm
@@ -0,0 +1,8 @@
+//
+//  UnityExternFunctions.mm
+//
+//  Created by Curtis Johnson on 2/25/14.
+//  Copyright (c) 2014 mallocation. All rights reserved.
+//
+
+void UnitySendMessage(const char* obj, const char* method, const char* msg) { }
